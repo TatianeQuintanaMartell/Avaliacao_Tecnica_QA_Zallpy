f9cb193e9f8f73d2b1dbc901f7fe5dc7079a4d29
Tatiane Quintana Martell <tatanequintanamartell@gmail.com>
2021-03-07 20:12:08 -0300

    Adicionando projeto ao repositorio

---
+++ src/main/java/Framework/Utils.java
@@ -0,0 +1,14 @@
+package Framework;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class Utils {
+    private Select select;
+
+    public void selecionarTexto(String properties, WebElement webElement){
+        select = new Select(webElement);
+        select.selectByVisibleText(FileOperation.getProperties("dadosDeEntradaParaoTeste").getProperty(properties));
+    }
+
+}
